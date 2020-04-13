add7ae5ed34d5af252bc25afdcd4f03d5e841bf0
Gregor <gregorlennie@hotmail.co.uk>
2020-04-13 11:44:07 +0100

    Fills recycler view with mock data

--- app/src/main/java/com/example/android/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/android/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -1,18 +1,26 @@
 package com.example.android.listmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+    val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_selection_view_holder, parent, false)
+
+        return ListSelectionViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listTitles.size
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.listPosition.text = (position + 1).toString()
+        holder.listTitle.text = listTitles[position]
     }
 }
